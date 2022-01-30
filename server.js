33772c62e3d9778e34699d958cc0296a81c7b13b
Ashif Nadaf <95206834+ashifnadaf@users.noreply.github.com>
2022-01-31 00:46:25 +0530

    Add files via upload
    first commit

---
+++ server.js
@@ -0,0 +1,88 @@
+var express = require("express");
+var cookie = require("cookie-parser");
+var bodyparser = require("body-parser");
+var multer = require("multer");
+
+
+var fs = require("fs");
+var app = express();
+
+app.use(bodyparser.json());
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.use(express.static("public"));
+app.listen(8081,function(){
+    console.log("server running at http://localhost:8081");
+})
+
+var header = fs.readFileSync("header.html");
+var footer = fs.readFileSync("footer.html");
+
+app.get("/index",function(req,res)
+{
+
+    fs.readFile("index.html",function(err,data)
+    {
+        res.send(header.toString() + data.toString() + footer.toString());
+    })
+})  
+app.get("/",function(req,res)
+{
+    
+    fs.readFile("index.html",function(err,data)
+    {
+        res.send(header.toString() + data.toString() + footer.toString());
+    })
+})
+app.get("/about",function(req,res)
+{
+    fs.readFile("about.html",function(err,data)
+    {
+        res.send(header.toString() + data.toString() + footer.toString());
+    })
+})
+app.get("/contact",function(req,res)
+{
+    fs.readFile("contact.html",function(err,data)
+    {
+        res.send(header.toString() + data.toString() + footer.toString());
+    })
+})
+app.get("/course-details",function(req,res)
+{
+    fs.readFile("course-details.html",function(err,data)
+    {
+        res.send(header.toString() + data.toString() + footer.toString());
+    })
+})
+app.get("/courses",function(req,res)
+{
+    fs.readFile("courses.html",function(err,data)
+    {
+        res.send(header.toString() + data.toString() + footer.toString());
+    })
+})
+app.get("/events",function(req,res)
+{
+    fs.readFile("events.html",function(err,data)
+    {
+        res.send(header.toString() + data.toString() + footer.toString());
+    })
+})
+app.get("/pricing",function(req,res)
+{
+    fs.readFile("pricing.html",function(err,data)
+    {
+        res.send(header.toString() + data.toString() + footer.toString());
+    })
+})
+app.get("/trainers",function(req,res)
+{
+    fs.readFile("trainers.html",function(err,data)
+    {
+        res.send(header.toString() + data.toString() + footer.toString());
+    })
+})
+
+
